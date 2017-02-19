b5f73c7a76dece240618537225d367f53c576f1c
4xDMG <dajtait87@gmail.com>
2017-02-19 17:06:11 +1000

    Set up file for GameBoard component

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,20 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <h2>Conway&#39;s Game of Life</h2>
+        </div>
+        <div>
+          Game board goes here
         </div>
       </div>
     );
